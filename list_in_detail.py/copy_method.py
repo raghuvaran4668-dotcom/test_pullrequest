a3692061ee564b57b7ccda631e9eaa5ef2fc04e3
raghu varma <raghuvaran4668@gmail.com>
2026-02-25 03:11:42 +0000

    Add README.md for pull request demo learn python in detail in lists funtion

---
+++ list_in_detail.py/copy_method.py
@@ -0,0 +1,4 @@
+orginal = ["raghuvendra", "sai", "kumar"]
+add_member = orginal.copy() # should create a copy of the original list
+add_member.append("rohith") # should add "rohith" to the list
+print(add_member) # should print ["raghuvendra", "sai", "kumar"]
---
+++ list_in_detail.py/reverse_list.py
@@ -0,0 +1,7 @@
+# create a function that takes a list as an argument and returns the list in reverse order
+def reverse_list(Lst):
+    return Lst[::-1]
+# test the function print(reverse_list([1, 2, 3, 4, 5])) # should return [5, 4, 3, 2, 1]
+print(reverse_list([1,2,3,4,5])) # should return [5, 4, 3, 2, 1]
+
+
